4d4a758a8058fe37234f1a22cea5aee2f27f2540
Torben <torbenrees@greavesbrewster.co.uk>
2017-07-06 11:15:14 +0100

    updated comic book dtail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,19 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "script: dan slott",
+                "pencils: humberto ramos",
+                "inks: victor olazaba",
+                "colors: edgar delgado",
+                "letters: chris eliopoulos"
+            };
+
             return View();
         }
     }
